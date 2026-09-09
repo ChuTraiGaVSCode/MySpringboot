23382718e9ae2f93d7eb3152525d9e4b290ad110
ChuTraiGaVSCode <tathanh1501@gmail.com>
2026-09-09 18:33:01 +0700

    first commit

---
+++ src/main/java/vn/iotstar/Springboot11Application.java
@@ -0,0 +1,13 @@
+package vn.iotstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot11Application.class, args);
+	}
+
+}
